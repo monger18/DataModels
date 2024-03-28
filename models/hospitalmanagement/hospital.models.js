33f86048f152590968fa6ef5f3927b7e2e725b9f
Satyam srinath <130518559+monger18@users.noreply.github.com>
2024-03-28 19:56:54 +0530

    Update hospital.models.js

--- models/hospitalmanagement/hospital.models.js
+++ models/hospitalmanagement/hospital.models.js
@@ -1,4 +1,27 @@
 import mongoose from 'mongoose'
-const hospitalSchema=new mongoose.Schema({},{timestamps:true})
+const hospitalSchema=new mongoose.Schema({
+  name:{
+        type:String,
+        required:true,
+    },
+    addressLine1:{
+        type:String,
+        required:true,
+    },
+    addressLine2:{
+        type:String,
+    },
+    city:{
+        type:String,
+        required:true,
+    },
+    pincode:{
+        type:String,
+        required:true,
+    },
+    specializedIn:[{
+        type:String,
+    }]
+},{timestamps:true})
 
 export const Hospitals=mongoose.model('Hospitals',hospitalSchema)
